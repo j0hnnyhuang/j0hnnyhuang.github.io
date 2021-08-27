6ee2e8cdfa4d3f4b883e10690b080d4489c06777
Jonathan Huang <huangj326@gmail.com>
2021-08-26 20:15:54 -0400

    Add footer section

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,9 +22,7 @@
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
-			window.setTimeout(function() {
 				$body.removeClass('is-preload');
-			}, 100);
 		});
 
 	// Nav.
